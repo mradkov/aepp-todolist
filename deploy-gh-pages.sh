2ca1eec6ce69d808dff9b28b4e5bfa91e25beca9
Milen Radkov <milenradkov@me.com>
2019-09-14 12:57:44 +0200

    update sdk and deploy script

--- deploy-gh-pages.sh
+++ deploy-gh-pages.sh
@@ -4,10 +4,10 @@ GIT_REV="$(git rev-parse HEAD)"&& \
 rm -rf dist && \
 rm -rf node_modules && \
 npm install && \
-npm run build && \
+NODE_ENV=prod npm run build && \
 cd dist/ && \
 git init && \
-git remote add origin git@github.com:thepiwo/aepp-todolist.git && \
+git remote add origin git@github.com:mradkov/aepp-todolist.git && \
 git checkout -b gh-pages && \
 git add * && \
 git commit -m "todolist aepp ${GIT_REV} deployment to gh-pages" && \
